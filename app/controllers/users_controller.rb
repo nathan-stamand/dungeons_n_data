3df777e41ceaadd76b12c6e758cac1e9eac28b5f
nathan-stamand <nathansaint810@gmail.com>
2020-05-27 13:59:27 -0500

    added private method #user_params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,10 @@ class UsersController < ApplicationController
     def create
         
     end
+
+    private 
+
+    def user_params 
+        params.require(:user).permit(:username, :email, :password)
+    end
 end
