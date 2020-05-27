2e5b7b37a78c839c98ebf8b6c5cc9ef2cf285179
nathan-stamand <nathansaint810@gmail.com>
2020-05-27 11:59:33 -0500

    added relationships for users, characters, and sessions

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -1,2 +1,9 @@
 class Campaign < ApplicationRecord
+    has_many :sessions
+    belongs_to :dungeon_master, 
+      :class_name => "User",
+      :foreign_key => :dungeon_master_id
+    has_many :players, 
+      :class_name => "User"
+    has_many :characters, through: :players
 end
