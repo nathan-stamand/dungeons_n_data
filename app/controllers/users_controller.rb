47d9a44ee5cea3d3bee6c1a5f4dd6ea21fb1d45b
nathan-stamand <nathansaint810@gmail.com>
2020-05-27 14:08:08 -0500

    added logic to #create - if @user saves, set session & redirect to their show page, else render new_user_path again.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,12 @@ class UsersController < ApplicationController
 
     def create
         @user = User.new(user_params)
-        binding.pry
+        if @user.save
+            session[:user_id] = @user.id 
+            redirect_to user_path(@user)
+        else
+            render new_user_path
+        end
     end
 
     private 
