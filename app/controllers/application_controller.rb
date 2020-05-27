4a0638eb99e2fc871710b0278a263904637393d8
nathan-stamand <nathansaint810@gmail.com>
2020-05-27 14:12:12 -0500

    added #current_user and #logged_in? methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+
+    def logged_in?
+        !!session[:user_id]
+    end
+
+    def current_user 
+        User.find_by(id: session[:user_id])
+    end 
 end
