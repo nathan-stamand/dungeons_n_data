527a305ad85a58d7d5e902f607ea7bbe2dba5047
nathan-stamand <nathansaint810@gmail.com>
2020-05-28 09:45:06 -0500

    updated #character_params to include :character_class

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -5,12 +5,11 @@ class CharactersController < ApplicationController
 
     def create 
         @character = Character.new(character_params)
-        
     end
 
     private 
 
     def character_params 
-        params.require(:character).permit(:name, :level, :class, :race)
+        params.require(:character).permit(:name, :level, :character_class, :race)
     end
 end
