cb8fca42a1abb9ab36fc990b38080bb2a38bde4a
nathan-stamand <nathansaint810@gmail.com>
2020-05-28 08:53:31 -0500

    added nested resources for users (characters and campaigns) and campaigns (sessions)

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,15 @@ Rails.application.routes.draw do
   get '/', to: "home#index"
   resources :characters
   resources :sessions
-  resources :campaigns
-  resources :users
+
+  resources :campaigns do 
+    resources :sessions 
+  end 
+
+  resources :users do
+    resources :characters
+    resources :campaigns
+  end
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
