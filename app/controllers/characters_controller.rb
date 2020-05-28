c979da07ebf50999f8b792374f201e4c4143238a
nathan-stamand <nathansaint810@gmail.com>
2020-05-28 09:17:24 -0500

    added #new and #create with @character variables

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,2 +1,16 @@
 class CharactersController < ApplicationController
+    def new
+        @character = Character.new
+    end
+
+    def create 
+        @character = Character.new(character_params)
+        
+    end
+
+    private 
+
+    def character_params 
+        params.require(:character).permit(:name, :level, :class, :race)
+    end
 end
