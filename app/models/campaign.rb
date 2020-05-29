7419cf5c5976eef176c8be69282f350466a23436
nathan-stamand <nathansaint810@gmail.com>
2020-05-29 13:00:25 -0500

    changed dungeon_master's foreign key to :user_id

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -2,7 +2,7 @@ class Campaign < ApplicationRecord
     has_many :sessions
     belongs_to :dungeon_master, 
       :class_name => "User",
-      :foreign_key => :dungeon_master_id
+      :foreign_key => :user_id
     has_many :player_campaigns
     has_many :players, through: :player_campaigns
     has_many :characters, through: :players
