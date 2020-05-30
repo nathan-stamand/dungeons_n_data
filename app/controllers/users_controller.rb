29cd8e9bdb861db3065d97946da295eb328dd45c
nathan-stamand <nathansaint810@gmail.com>
2020-05-30 16:09:43 -0500

    restructured logic in #login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,11 +21,11 @@ class UsersController < ApplicationController
     def login
         if !!params[:user]
             @user = User.find_by(username: params[:user][:username])
-            if @user.authenticate(params[:user][:password])
-                session[:user_id] = @user.id 
-                redirect_to user_path(@user)
-            else 
+            if !@user
                 render users_login_path
+            elsif @user.authenticate(params[:user][:password])
+                session[:user_id] = @user.id 
+                redirect_to user_path(@user)    
             end
         else 
             @user = User.new
