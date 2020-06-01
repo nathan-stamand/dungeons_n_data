f5bb3461c394c9b65da0ac6075c38aeeb37497cd
nathan-stamand <nathansaint810@gmail.com>
2020-06-01 11:48:14 -0500

    added argument for params to be pulled into #find_hours

--- app/models/session.rb
+++ app/models/session.rb
@@ -1,11 +1,11 @@
 class Session < ApplicationRecord
     belongs_to :campaign
 
-    def find_hours
+    def find_hours(params)
         @start = params[:session][:start_time].to_time
         @end = params[:session][:end_time].to_time
         @hours = ((@end - @start).to_f/3600).to_f
         self.hours = @hours
     end
-    
+
 end
