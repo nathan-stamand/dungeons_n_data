a66928f365e5fa2775d70071c91521115ae221a2
nathan-stamand <nathansaint810@gmail.com>
2020-06-01 11:39:46 -0500

    updated schema for new migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_01_160148) do
+ActiveRecord::Schema.define(version: 2020_06_01_162916) do
 
   create_table "campaigns", force: :cascade do |t|
     t.string "title"
@@ -36,12 +36,13 @@ ActiveRecord::Schema.define(version: 2020_06_01_160148) do
 
   create_table "sessions", force: :cascade do |t|
     t.string "place"
-    t.datetime "start_time"
+    t.string "start_time"
     t.integer "hours"
     t.integer "campaign_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.datetime "end_time"
+    t.string "end_time"
+    t.string "date"
   end
 
   create_table "users", force: :cascade do |t|
