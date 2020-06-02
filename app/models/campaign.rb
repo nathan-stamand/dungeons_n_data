366534a1e49f01dae3b13c7d8c6c082c0b1f8972
nathan-stamand <nathansaint810@gmail.com>
2020-06-02 11:19:44 -0500

    added #add_player method to create an association between a player and campaign

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -7,4 +7,12 @@ class Campaign < ApplicationRecord
     has_many :players, through: :player_campaigns
     has_many :characters, through: :players
     validates :title, presence: true
+
+  def add_player(params)
+    if params[:campaign][:players]
+        @player = User.find_by(id: params[:campaign][:players])
+        @join = PlayerCampaign.create(user_id: @player.id, campaign_id: self.id)
+    end
+  end
+
 end
