d701ab5b8ae01ced0cd03bce42fcf4365e424826
nathan-stamand <nathansaint810@gmail.com>
2020-06-03 18:19:11 -0500

    added scopes for alive, bloodied, and critical conditions

--- app/models/character.rb
+++ app/models/character.rb
@@ -2,4 +2,7 @@ class Character < ApplicationRecord
     belongs_to :player,
     :class_name => "User",
     :foreign_key => :user_id
+    scope :alive, -> {where("current_hit_points > 0")}
+    scope :bloodied, -> {alive.where("current_hit_points <= (max_hit_points * .5)")}
+    scope :critical, -> {where("current_hit_points == 0")}
 end
