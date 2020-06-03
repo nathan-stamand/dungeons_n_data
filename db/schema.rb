65277743e6361ca49ae7bfd9c7bb6ed50aa7244b
nathan-stamand <nathansaint810@gmail.com>
2020-06-03 16:35:14 -0500

    added uid column to users table, changed name of sessions table to dnd_sessions for clarity

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_03_142517) do
+ActiveRecord::Schema.define(version: 2020_06_03_204709) do
 
   create_table "campaigns", force: :cascade do |t|
     t.string "title"
@@ -34,12 +34,7 @@ ActiveRecord::Schema.define(version: 2020_06_03_142517) do
     t.integer "damage"
   end
 
-  create_table "player_campaigns", force: :cascade do |t|
-    t.integer "user_id"
-    t.integer "campaign_id"
-  end
-
-  create_table "sessions", force: :cascade do |t|
+  create_table "dnd_sessions", force: :cascade do |t|
     t.string "place"
     t.string "start_time"
     t.string "hours"
@@ -51,12 +46,18 @@ ActiveRecord::Schema.define(version: 2020_06_03_142517) do
     t.string "dm_notes"
   end
 
+  create_table "player_campaigns", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "campaign_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "uid"
   end
 
 end
