5208eec0f7a1b155af928e37258bffa96f0e424c
nathan-stamand <nathansaint810@gmail.com>
2020-06-05 14:51:31 -0500

    set up helper methods for all methods, and a before_action to call #current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
+    helper_method :logged_in?
+    helper_method :current_user
+    helper_method :form_check
+    helper_method :check
+    before_action :current_user
+
     private
 
     def logged_in?
