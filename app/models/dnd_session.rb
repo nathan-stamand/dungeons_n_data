ee298d5a286c5e5c0709682b70ba0cf8daf29d82
nathan-stamand <nathansaint810@gmail.com>
2020-06-05 08:28:15 -0500

    added recently_changed, recently_made, and with_notes scopes to find DndSessions that were created/updated in the past 30 days or  that have a value for dm_notes

--- app/models/dnd_session.rb
+++ app/models/dnd_session.rb
@@ -1,5 +1,8 @@
 class DndSession < ApplicationRecord
     belongs_to :campaign
+    scope :recently_changed, -> {where("updated_at > ?", Time.now - 2592000)}
+    scope :recently_made, -> {where("created_at > ?", Time.now - 2592000)}
+    scope :with_notes, -> {where.not(dm_notes: [nil, ''])}
 
     def find_hours(params)
         @start = params[:dnd_session][:start_time].to_time
