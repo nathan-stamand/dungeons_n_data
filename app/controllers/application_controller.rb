8b59810b407a09d582c7ca474ffac5552681764f
nathan-stamand <nathansaint810@gmail.com>
2020-06-05 14:50:13 -0500

    set all methods to private, changed #current_user to set the current user to @user variable, added #check and #form_check to chelp with views either displaying forms or redirecting users without access

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,4 @@
 class ApplicationController < ActionController::Base
-    helper_method :logged_in?
-    helper_method :current_user
-    helper_method :form_check
-    helper_method :check
-    before_action :current_user
-
     private
 
     def logged_in?
