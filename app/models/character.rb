59a2909728a5c6906b4ef66bd6ba903dffaad4c3
nathan-stamand <nathansaint810@gmail.com>
2020-06-08 11:46:50 -0500

    removed scopes for :alive, :bloodied, and :critical

--- app/models/character.rb
+++ app/models/character.rb
@@ -2,9 +2,6 @@ class Character < ApplicationRecord
     belongs_to :player,
     :class_name => "User",
     :foreign_key => :user_id
-    scope :alive, -> {where("current_hit_points > 0")}
-    scope :bloodied, -> {alive.where("current_hit_points <= (max_hit_points * .5)")}
-    scope :critical, -> {where("current_hit_points == 0")}
 
     def status
         if current_hit_points < 1 
