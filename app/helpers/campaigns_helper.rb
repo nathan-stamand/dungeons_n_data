059e64ea6b11f1bdcb790d3cb34bdbc06de363b4
nathan-stamand <nathansaint810@gmail.com>
2020-06-12 12:07:24 -0500

    created #player_select to make a collection of arrays with username/id from #userlist and created #select_box_number to return number for creating select boxes

--- app/helpers/campaigns_helper.rb
+++ app/helpers/campaigns_helper.rb
@@ -2,4 +2,12 @@ module CampaignsHelper
   def user_list
     User.all.find_all {|user| user if user != current_user}
   end
+
+  def player_select 
+    user_list.collect{|user| [user.username, user.id]}
+  end
+
+  def select_box_number(campaign)
+    @campaign.players.length + 3 || 3
+  end
 end
