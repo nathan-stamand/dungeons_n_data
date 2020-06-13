4701946511269911ef2814e944d55cd3cb039872
nathan-stamand <nathansaint810@gmail.com>
2020-06-13 10:54:50 -0500

    deleted #login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,25 +18,6 @@ class UsersController < ApplicationController
     @user = User.find_by(id: params[:id])
   end
 
-  def login
-    if !!params[:user]
-      @user = User.find_by(username: params[:user][:username])
-    if !@user
-      render users_login_path
-    elsif @user.authenticate(params[:user][:password])
-      session[:user_id] = @user.id 
-      redirect_to user_path(@user)
-    end
-    else
-      @user = User.new
-    end
-  end
-
-  def logout
-    session.clear
-    redirect_to root_path
-  end
-
   private
 
   def user_params
