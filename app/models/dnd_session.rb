d1d3bc1a266adc175b5b29b9e4e969fa7f2cedf2
nathan-stamand <nathansaint810@gmail.com>
2020-06-12 19:08:45 -0500

    minor spacing reformatting, removed unnecessary 'self'

--- app/models/dnd_session.rb
+++ app/models/dnd_session.rb
@@ -12,7 +12,6 @@ class DndSession < ApplicationRecord
     @start = params[:dnd_session][:start_time].to_time
     @end = params[:dnd_session][:end_time].to_time
     @hours = ((@end - @start).to_f/3600).to_f.round(2)
-    self.hours = @hours.to_s
+    hours = @hours.to_s
   end
-
 end
