0554cf507910a18f912bfe71e5f7928b7e7fe152
nathan-stamand <nathansaint810@gmail.com>
2020-09-23 09:15:43 -0500

    Remove require jquery lines

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,9 +4,6 @@
 // that code so it'll be compiled.
 
 require("@rails/ujs").start()
-require("jquery")
-require("jquery_ujs")
-
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
